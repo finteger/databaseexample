40066c4ceb880c36590e3d2a2c64f9085d3e7acf
finteger <104397474+finteger@users.noreply.github.com>
2023-03-14 08:44:32 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -6,23 +6,38 @@ var database = require('./database.json');
 var app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static('public')); //visit localhost:3000/images/netflix.png for static files
 
 
-app.get('/all', (req, res) => {
+app.get('/:id', (req, res) => {
   filePath = './database.json/';
   fs.readFile(filePath, function (err, data) {
     if (err) {
             throw err;
      }  
-     var jsondata = JSON.parse(data);
+
+   
+    var jsondata = JSON.parse(data);
+
+     //JSON.parse()  - create an object to access properties with "." or DOT notation
+
+     //JSON.stringify - creates useable JSON-formatted data that you can display from an object
+
+     if(!jsondata.users[req.params.id]){
+
+      res.status(404);
+      res.send(404);
+     }
      
-     res.send(jsondata.users);
+     res.send(jsondata.users[req.params.id]);
    });
 });
 
 app.get('/', (req, res) => {
-res.render('home.ejs');
+var data = {
+  "username": "john"
+}
+res.render('home.ejs', {data});
 });
 
 
@@ -38,7 +53,7 @@ app.delete('/delete/:id', (req, res) => {
 
      delete user;
 
-     var usertoString = JSON.stringify(user);
+     var usertoString = JSON.stringify(user.username);
 
      console.log(usertoString);
 
